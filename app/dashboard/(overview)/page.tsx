a238ec9c1dc9779e664c22d19a47adf5b6d7f61e
daussfajar <daussfajar28@gmail.com>
2024-09-24 17:01:59 +0700

    update

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -38,7 +38,7 @@ export default async function Page() {
                     <RevenueChart revenue={[]} />
                 </Suspense>
                 <Suspense fallback={<LatestInvoicesSkeleton />}>
-                    <LatestInvoices latestInvoices={[]} />
+                    <LatestInvoices latestInvoices={[]}/>
                 </Suspense>
             </div>
         </main>
